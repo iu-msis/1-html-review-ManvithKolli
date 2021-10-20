859238452397185f2fdfc361b018c9f1551b435f
ManvithKolli <apexpioneer@outlook.com>
2021-10-20 18:09:58 -0400

    Update Changes

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,12 +1,16 @@
 const BooksData = {
     data() {
       return {
-        "books":[]
+        "books":[],
+        students: [],
+        selectedStudent: null,
+        offers: [],
+        offerForm: {},
+        selectedOffer: null
        }
     },
     
     methods:{
-
       fetchBooksData() {
         fetch('/api/books/')
         .then( response => response.json() )
@@ -17,7 +21,33 @@ const BooksData = {
         .catch( (err) => {
             console.error(err);
         })
-    }
+    },
+    postOffer(evt) {
+      console.log("Sample Log- Control is Here")
+      this.postNewOffer(evt);
+  },
+  postNewOffer(evt) {
+    //this.offerForm.studentId = this.selectedStudent.id;        
+    
+    console.log("Creating!", this.offerForm);
+
+    fetch('api/books/create.php', {
+        method:'POST',
+        body: JSON.stringify(this.offerForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => {
+        console.log("Returned from post:", json);
+        // TODO: test a result was returned!
+        this.offers = json;
+        
+        // reset the form
+        this.handleResetEdit();
+      });
+  },
     },
 
     created(){
